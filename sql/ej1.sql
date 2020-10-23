88ffda1286190174fd58a2cd2e3865ff1b6d3c9b
carlmrx <carlos_s.c@hotmail.es>
2020-10-22 23:10:04 -0500

    .

---
+++ sql/ej1.sql
@@ -0,0 +1,12 @@
+create database blog;
+
+use blog;
+
+create table usuarios(
+    id  int(11),
+    nombre varchar(100),
+    apellodos varchar(100),
+    email varchar(100),
+    password varchar(100)
+);
+
