384ddcd1302fd88cef4b4933ebb377f5aa6ad264
carlmrx <carlos_s.c@hotmail.es>
2020-10-23 10:55:39 -0500

    .

--- sql/ej1.sql
+++ sql/ej1.sql
@@ -15,4 +15,16 @@ create table usuarios(
 drops table usuarios;
 * eliminar bd *
 drop blog
- */
+*renombrar una tabla*
+alter table usuarios rename to usuarios_renom;
+* cambiar nombre de una columna*
+alter table usuarios_renom change apellodos apellidos varchar(100) null;
+*modificar columna sin cambiar el nombre* 
+alter table usuarios_renom MODIFY apellido char(40) not null;
+*añadir una columna*
+alter table usuarios_renom add COLUMN website vrachar(100) null;
+*añadir sertricciones a columnas*
+alter table usuarios_renom add constraint unq_mails unique(email);
+*borrar una columna*
+alter table usuarios_renom drop website:
+*/
