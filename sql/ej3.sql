d8fa2c78fc1065560b3f7d7a0910273575dcd88f
carlmrx <carlos_s.c@hotmail.es>
2020-10-23 13:26:22 -0500

    .

---
+++ sql/ej3.sql
@@ -0,0 +1,14 @@
+/* use ej2;
+*insertar basico*
+insert into usuarios VALUES(3,'carlos','serrano','carlos@carlos.com','123abc','2019-05-01');
+insert into usuarios VALUES(4,'juan','perez','juan@juan.com','123abc','2019-05-03');
+insert into usuarios VALUES(5,'jose','hernandes','jose@jose.com','123abc','2019-05-04');
+insert into usuarios VALUES(6,'alejandro','salgado','alejandro@alejanro.com','123abc','2019-05-05');
+*insertar selectivo*
+insert into usuarios(id,nombre,apellidos) VALUES(8,'alejandro','salgado');
+*visualizar datos*
+*ver todos*
+select * from usuarios;
+*seleccionar que ver*
+select nombre from usuarios;
+select nombre,apellidos from usuarios; */
