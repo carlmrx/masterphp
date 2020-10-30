17c0bbfa0e045a91708bd504866dce57c6cf9e06
carlmrx <carlos_s.c@hotmail.es>
2020-10-30 00:04:02 -0600

    .

---
+++ tiendita/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '59ecbbbfdc437d3cc2032512aec512ae80aa357c',
+    'name' => 'tiendita/online',
+  ),
+  'versions' => 
+  array (
+    'tiendita/online' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '59ecbbbfdc437d3cc2032512aec512ae80aa357c',
+    ),
+  ),
+);
